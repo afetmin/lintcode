73be6d088139c1a4b271835bf90c1143a67c6641
yilun <kingpenguins@163.com>
2017-11-19 22:05:05 +0800

    测试提交

--- 第一个不重复的字符.py
+++ 第一个不重复的字符.py
@@ -2,9 +2,9 @@
 #coding: utf-8
 s= 'lintcodelintcode'
 for i,element in enumerate(s):
-	if s.count(element) > 1:
-		print -1
-		break
-	else:
-		break
+    if s.count(element) > 1:
+        print -1
+        break
+    else:
+        break
 print i
